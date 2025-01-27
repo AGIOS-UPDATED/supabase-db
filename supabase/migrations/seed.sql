721462b90e54a0620580c862293409cc91a5c0a8
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-01-27 10:02:45 +0500

    Added

--- supabase/migrations/seed.sql
+++ supabase/migrations/seed.sql
@@ -3,10 +3,6 @@ SELECT vault.create_secret('http://localhost:3001/api', 'WEBHOOK_ENDPOINT', 'Web
 SELECT vault.create_secret('6c369443-1a88-444e-b459-7e662c1fff9e', 'WEBHOOK_SECRET', 'Webhook secret key');
 
 
-INSERT INTO secrets (name, value)
-VALUES ('WEBHOOK_ENDPOINT', 'new_value')
-ON CONFLICT (name)
-DO UPDATE SET value = EXCLUDED.value;
 
 
 7d3f4c8d-122e-4c91-b9ed-0c4e3966d476
